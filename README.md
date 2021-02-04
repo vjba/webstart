eed7e31bdb779747a1b50a69855eac2373175d1d
vjba <21375278+vjba@users.noreply.github.com>
2021-02-04 00:30:11 +0000

    New style (#5)
    * add favicon
    
    * fix indent
    
    * bump version
    bump bootstrap to beta
    
    * update style
    add icon license
    add copy button

---
+++ README.md
@@ -0,0 +1,5 @@
+# Webstart
+
+## Credits
+
+1. `globe-solid.svg` &copy; FontIcons, Inc. [License](https://fontawesome.com/license) CC BY 4.0
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,6 @@
-import { bootstrap } from 'bootstrap'
+copyRepo = () => {
+    navigator.clipboard.writeText('git@github.com:vjba/webstart.git')
+    let copyButton = document.getElementById('copyRepoLink')
+    copyButton.innerHTML = 'done'
+    copyButton.classList.add('disabled')
+}
